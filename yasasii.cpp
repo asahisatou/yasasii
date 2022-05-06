14d3d94af2ea9e5e1ca9534f9104fbc0ddc33b45
asahisatou <88087478+asahisatou@users.noreply.github.com>
2022-05-07 04:41:21 +0900

    5/7 4:41

--- yasasii.cpp
+++ yasasii.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 using namespace std;
+
+template <class T>
+T squaret(T x)
+{
+    T ans = x * x;
+    return ans;
+}
+
 int main()
 {
-    int score;
-    int total = 0;
-    cout << "テスト点数を入力してください。\n";
+    int a;
+    double da;
 
-    while(score){
-        cin >> score;
-        total+=score;
-    }
-    cout << "テストの点数は" << total << "点です。\n";
+    cout << "整数を入力してください。\n";
+    cin >> a;
+
+    cout << "少数を入力してください。\n";
+    cin >> da;
+
+    int ans1 = squaret(a);
+    double ans2 = squaret(da);
+
+    cout << "整数の二乗は" << ans1 << "です。\n";
+    cout << "少数の二乗は" << ans2 << "です。\n";
+
     return 0;
 }
